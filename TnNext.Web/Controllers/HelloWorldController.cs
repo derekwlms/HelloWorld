65eee8e2d6d8124781e57745ac4741f83705df8d
Cedric <cedric@Cedrics-MacBook-Pro.local>
2019-05-06 15:18:40 -0400

    Initial Commit

---
+++ TnNext.Web/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TnNext.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Hello World.";
+        }
+    }
+}
