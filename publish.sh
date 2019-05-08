bd1c58a39ac1068d00439222c898f2931effe96f
Cedric <cedric@Cedrics-MacBook-Pro.local>
2019-05-07 23:22:58 -0400

    updated publish to output directory

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-rm -rf ../build-artifacts
+rm -rf ../../build-artifacts
 dotnet clean
-dotnet publish -o ../build-artifacts -r linux-x64 ./TnNext.Web/TnNext.Web.csproj
+dotnet publish -o ../../build-artifacts -r linux-x64 ./TnNext.Web/TnNext.Web.csproj
